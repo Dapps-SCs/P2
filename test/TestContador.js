25391c66789ee4edd83e696089d7f9150ed27160
Adrián <ablazleon@gmail.com>
2020-11-25 10:00:39 +0100

    Update TestContador.js

--- test/TestContador.js
+++ test/TestContador.js
@@ -40,6 +40,47 @@ contract('Usamos un Contador:', accounts => {
       assert.equal(incr.toNumber(), 1, "El incremento del valor no es 1.");
     });
   });
+  
+    it("decrementar en uno el contador", () => {
+
+    let c1, c2;
+
+    return contador.valor.call()
+    .then(value => {
+      c1 = value;
+      return contador.decr(); 
+    })
+    .then(() => contador.valor.call() )
+    .then(value => {
+      c2 = value;
+
+      const decremento = c2.sub(c1);
+      assert.equal(decremento.toNumber(), 1, "El decremento del valor no es 1.");
+    });
+  });
+  
+  it("decrementa en cuatro el contador", async () => {
+
+    let c1 = await contador.valor.call();
+    await contador.decr(); 
+    await contador.decr(); 
+    await contador.decr(); 
+    await contador.decr(); 
+    let c2 = await contador.valor.call();
+ 
+    const decremento = c2.sub(c1);
+    assert.equal(decremento.toNumber(), 4, "El decremento del valor no es 4.");
+  });
+  
+    it("resetea el contador", async () => {
+
+    let c1 = await contador.valor.call();
+    await contador.reset(); 
+ 
+    const valor = await contador.valor.call();
+    assert.equal(valor.toNumber(), 0, "El valor no es 0.");
+  });
+
 
 
   it("incrementa en cuatro el contador", async () => {
